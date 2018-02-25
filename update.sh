57ebbc2dfcd2a9d40e2fe9798dc79690e6c539a2
oleblaesing <ole.blaesing@web.de>
2018-02-25 15:23:06 +0100

    Add autoremove to update script

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/zsh
+brew update && brew upgrade
+brew cleanup
+upgrade_oh_my_zsh
 git -C ~/Code/dotfiles pull
 source ~/.zshrc
-brew update && brew upgrade
-upgrade_oh_my_zsh
-curl http://wttr.in/cologne\?1nQ
