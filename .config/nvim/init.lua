359848fce571b29fd9f02652e1a7e2cf23188165
Ole Bläsing <oleblaesing@users.noreply.github.com>
2023-03-03 22:32:06 +0100

    Use null-ls for eslint and formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -172,6 +172,33 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    opts = function(_, opts)
+      local null_ls = require('null-ls')
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      opts.sources = {
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.rustfmt,
+      }
+
+      opts.on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end
+    end,
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
