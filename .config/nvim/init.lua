dabba789fb7872b74a28f7f5a99cac6afc33f860
Ole Bläsing <oleblaesing@users.noreply.github.com>
2023-03-07 23:12:48 +0100

    Use hidden files for fzf usages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -313,10 +313,18 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function()
+  require('telescope.builtin').find_files({ hidden = true, file_ignore_patterns = { 'git' } })
+end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sg', function()
+  require('telescope.builtin').live_grep({
+    additional_args = function()
+      return { "--hidden" }
+    end,
+  })
+end, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
